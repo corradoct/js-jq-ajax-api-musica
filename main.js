15990df8a36f37f4b1628dcbb110ecb16afc5d01
Corrado <corradoct@hotmail.it>
2020-06-26 17:57:16 +0200

    Ho aggiunto il bonus

--- main.js
+++ main.js
@@ -16,16 +16,37 @@ $(document).ready(function() {
       url: 'https://flynn.boolean.careers/exercises/api/array/music',
       method: 'GET',
       success: function(dataResponse) {
-        var disco = dataResponse.response;
+        var cds = dataResponse.response;
 
         var source = document.getElementById("entry-template").innerHTML;
         var template = Handlebars.compile(source);
 
-        for (var i = 0; i < disco.length; i++) {
-          var context = disco[i];
+        for (var i = 0; i < cds.length; i++) {
+          var context = cds[i];
           var html = template(context);
           $('.cds-container').append(html);
         }
+
+        $('#genreSelect').on('change',
+          function() {
+            var selectedGenre = $(this).val();
+            console.log(selectedGenre);
+
+            if (selectedGenre === '') {
+              $('.cd').removeClass('hidden');
+            } else {
+              $('.cd').each(
+                function() {
+                  if ($(this).find('.genre').text().toLowerCase() === selectedGenre) {
+                    $(this).removeClass('hidden');
+                  } else {
+                    $(this).addClass('hidden');
+                  }
+                }
+              );
+            }
+          }
+        );
       },
       error: function() {
         alert('C\'è stato un\'errore');
